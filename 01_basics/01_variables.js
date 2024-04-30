a390220be6097bc1f7f5bcdadb3644214e30ef5c
Nishant Goutam <90135561+Nishant-Goutam@users.noreply.github.com>
2024-04-30 12:52:10 +0000

    variables Practices

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 1442;
+let accountEmail = "nishu70555@gmail.com";
+var accountPassword = "12345";
+accountCity = "jaipur";
+accountEmail = "nishant.com";
+accountPassword = "1234567890"
+// accountId = 2;
+
+console.log(accountPassword)
